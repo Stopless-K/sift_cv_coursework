1052f3f4925876eb5c9d036e29a9e1a583084cd4
Stopless-K <chen2008xin008@163.com>
2018-06-04 18:39:41 +0800

    add python sift file

---
+++ main.py
@@ -0,0 +1,6 @@
+from siftmatch import match_template
+
+if __name__ == '__main__':
+
+    path = '/Users/megvii/Downloads/vision_coursework/'
+    match_template(path+ '1.JPG', path+ '2.JPG', 10, 10)
